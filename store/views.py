55d4af38f548d4cda8fa253a1b89598ee26a4741
Bahi4130 <bahenskyt@gmail.com>
2021-11-28 18:04:20 +0100

    Add basic Store page

--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from category.models import Category
+from store.models import Product
+
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+    else:
+        products = Product.objects.all().filter(is_available=True)
+
+    context = {'products': products}
+    return render(request, 'store/store.html', context=context)
